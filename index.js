4c99a683344b521091019264c408b5b5e7b7d715
Vishal singh <vishalrajput862@gmail.com>
2024-05-16 14:49:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Package.json");
+const http= require("https");
+
+
+http.createServer((req,resp)=>{
+
+    resp.write("Hello this is vishal singh");
+    resp.end();
+
+}).listen(4500);
